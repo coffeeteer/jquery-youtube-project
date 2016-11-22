3ba59a868b4a34396e1cc757b008b80e2afa107b
Coffeeteer <tgrieshiemer@gmail.com>
2016-11-21 21:30:50 -0500

    found errors and corrected it... mostly

--- js/script.js
+++ js/script.js
@@ -1,67 +1,65 @@
-//Searchbar Handler
-$(document).ready(function(){
+// Searchbar Handler
+$(function(){
 	var searchField = $('#query');
 	var icon = $('#search-btn');
-
-	//Focus Handler
-	$(searchField).on('focus', function() {
+	
+	// Focus Event Handler
+	$(searchField).on('focus', function(){
 		$(this).animate({
-			width: '100%'
-		}, 400);
+			width:'100%'
+		},400);
 		$(icon).animate({
-			right: '10px',
+			right: '10px'
 		}, 400);
 	});
-
-	//Blur Event Handler
-	$(searchField).on('blur', function() {
-		if(searchField.val() == '') {
+	
+	// Blur Event Handler
+	$(searchField).on('blur', function(){
+		if(searchField.val() == ''){
 			$(searchField).animate({
-				width: '45%'
-			}, 400, function(){});
+				width:'45%'
+			},400, function(){});
 			$(icon).animate({
-				right: '360px'
-			}, 400, function(){});
+				right:'360px'
+			},400, function(){});
 		}
 	});
-
-	$('#search-form').submit(function() {
+	
+	$('#search-form').submit(function(e){
 		e.preventDefault();
 	});
-});
+})
+
 
 function search(){
-		// clear results	
-		$('#results').html('');
-		$('#buttons').html('');
-
-		//Get Form Input
-		q = $('#query').val(); 
-	}
-
-//Run GET Request on API
+	// Clear Results
+	$('#results').html('');
+	$('#buttons').html('');
+	
+	// Get Form Input
+	q = $('#query').val();
+	
+	// Run GET Request on API
 	$.get(
-		"https://www.googleapis.com/youtube/v3/search", 
-			{
-				part: 'snippet, id',
-				q: q, 
-				type: 'video',
-				key: 'AIzaSyBP8SomAQH4G--qPq-iSMLdEuEQg-NIDlo'
-			}, 
-			function(data) {
+		"https://www.googleapis.com/youtube/v3/search",{
+			part: 'snippet, id',
+			q: q,
+			type:'video',
+			key: 'AIzaSyBP8SomAQH4G--qPq-iSMLdEuEQg-NIDlo'},
+			function(data){
 				var nextPageToken = data.nextPageToken;
 				var prevPageToken = data.prevPageToken;
-
+				
 				// Log Data
 				console.log(data);
-
-				// $.each(data.items, function(i, item){
-					// Get Ouput -- comment --
-					//var output = getOuput(item);	
-
-					//Display results --Actually a comment --
-					//$('#results').append(output);
-				//}); 
+				
+				$.each(data.items, function(i, item){
+					// Get Output
+					var output = getOutput(item);
+					
+					// Display Results
+					$('#results').append(output);
+				});
 			}
-		);
-	
+	);
+}
